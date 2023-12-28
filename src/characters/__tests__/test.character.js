36c8601ff70b2735e5d331a88b7eecd7864395de
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:31:33 +0500

    Update test.character.js

--- src/characters/__tests__/test.character.js
+++ src/characters/__tests__/test.character.js
@@ -2,7 +2,7 @@ import Character from '../character';
 import Bowerman from '../bowerman';
 
 test('error', () => {
-  const person = new Bowerman('luba', 'Bowman');
+  const person = new Bowerman('dany', 'Bowman');
   person.health = 0;
   expect(() => person.levelUp()).toThrow(new Error('Нельзя повысить level умершего!'));
 });
@@ -13,18 +13,18 @@ test('error name', () => {
 });
 
 test('error name', () => {
-  const person = () => new Character('lubavushka123', 'Bowman');
+  const person = () => new Character('danylushka123', 'Bowman');
   expect(person).toThrow(new Error('Имя должно быть строкой от 2 до 10 символов'));
 });
 test('error type', () => {
-  const person = () => new Character('luba', 'Boooman');
+  const person = () => new Character('dany', 'Boooman');
   expect(person).toThrow(new Error('Тип указан некорректно!'));
 });
 
 test('damage', () => {
-  const person = new Bowerman('luba', 'Bowman');
+  const person = new Bowerman('dany', 'Bowman');
   person.damage(20);
   expect(person).toEqual({
-    health: 85, level: 1, attack: 25, defence: 25, name: 'luba', type: 'Bowman',
+    health: 85, level: 1, attack: 25, defence: 25, name: 'dany', type: 'Bowman',
   });
 });
