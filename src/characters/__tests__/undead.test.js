3d7327519c9a1f80ead467a7cfad8821f4a91d13
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:32:12 +0500

    Update undead.test.js

--- src/characters/__tests__/undead.test.js
+++ src/characters/__tests__/undead.test.js
@@ -1,16 +1,16 @@
 import Undead from '../undead';
 
 test('instance of the class Undead', () => {
-  const person = new Undead('luba');
+  const person = new Undead('dany');
   expect(person).toEqual({
-    health: 100, level: 1, attack: 25, defence: 25, type: 'Undead', name: 'luba',
+    health: 100, level: 1, attack: 25, defence: 25, type: 'Undead', name: 'dany',
   });
 });
 
 test('levelUp for class', () => {
-  const person = new Undead('luba');
+  const person = new Undead('dany');
   person.levelUp();
   expect(person).toEqual({
-    health: 100, level: 2, attack: 30, defence: 30, name: 'luba', type: 'Undead',
+    health: 100, level: 2, attack: 30, defence: 30, name: 'dany', type: 'Undead',
   });
 });
