600563dea2356e620806b506f9cacc992d59a0c7
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:28:45 +0500

    Update magician.test.js

--- src/characters/__tests__/magician.test.js
+++ src/characters/__tests__/magician.test.js
@@ -1,16 +1,16 @@
 import Magician from '../magician';
 
 test('instance of the class Magician', () => {
-  const person = new Magician('luba');
+  const person = new Magician('dany');
   expect(person).toEqual({
-    health: 100, level: 1, attack: 10, defence: 40, type: 'Magician', name: 'luba',
+    health: 100, level: 1, attack: 10, defence: 40, type: 'Magician', name: 'dany',
   });
 });
 
 test('levelUp for class', () => {
-  const person = new Magician('luba');
+  const person = new Magician('dany');
   person.levelUp();
   expect(person).toEqual({
-    health: 100, level: 2, attack: 12, defence: 48, name: 'luba', type: 'Magician',
+    health: 100, level: 2, attack: 12, defence: 48, name: 'dany', type: 'Magician',
   });
 });
