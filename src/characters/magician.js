b7d9ab8f81e0a45ea4367c9ef3589bd981f33bf5
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 11:29:42 +0500

    Update magician.js

--- src/characters/magician.js
+++ src/characters/magician.js
@@ -2,6 +2,8 @@ import Character from './character';
 
 class Magician extends Character {
     constructor(name) {
-        super(name, 10, 40);
+        super(name);
+        this.attack = 10;
+        this.defence = 40;
     }
 }
