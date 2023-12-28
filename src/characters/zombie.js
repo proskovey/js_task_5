b6e8f9fbc3930ba1b1045ef37768a2920c716716
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:16:25 +0500

    Update zombie.js

--- src/characters/zombie.js
+++ src/characters/zombie.js
@@ -1,9 +1,9 @@
 import Character from './character';
 
-class Zombie extends Character {
-    constructor(name) {
-        super(name);
-        this.attack = 40;
-        this.defence = 10;
-    }
+export default class Zombie extends Character {
+  constructor(name, type = 'Zombie') {
+    super(name, type);
+    this.attack = 40;
+    this.defence = 10;
+  }
 }
