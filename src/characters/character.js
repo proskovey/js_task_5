2008a1bc0a2798e7565a8a47dddc858722176907
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:13:42 +0500

    Update character.js

--- src/characters/character.js
+++ src/characters/character.js
@@ -1,39 +1,37 @@
 export default class Character {
-  constructor(name, attack, defence) {
-    this.name = name;
-    this.type = this.__proto__.constructor.name;
+  constructor(name, type) {
+    const types = ['Bowman', 'Swordsman', 'Magician', 'Daemon', 'Undead', 'Zombie'];
     this.health = 100;
     this.level = 1;
-    
     this.attack = undefined;
     this.defence = undefined;
-  }
 
-  get name() {
-    return this._name;
-  }
-
-  set name(name) {
-    if (name.length < 2 || name.length > 10) {
-      throw new Error('Имя должно быть от 2 до 10 символов');
+    if (!types.includes(type)) {
+      throw new Error('Тип указан некорректно!');
+    } else {
+      this.type = type;
     }
-    this._name = name;
+    if (name.length < 2 || name.length > 10) {
+      throw new Error('Имя должно быть строкой от 2 до 10 символов');
+    } else {
+      this.name = name;
+    }
   }
 
   levelUp() {
-    if (this.health > 0) {
-      this.level += 1;
-      this.attack *= 1.2;
-      this.defence *= 1.2;
-      this.health = 100;
+    if (this.health === 0) {
+      throw new Error('Нельзя повысить level умершего!');
     } else {
-      throw new Error('Нельзя повысить level умершего');
+      this.level += 1;
+      this.attack += ((this.attack / 100) * 20);
+      this.defence += ((this.defence / 100) * 20);
+      this.health = 100;
     }
   }
 
   damage(points) {
     if (this.health >= 0) {
-      this.health -= points * (1 - this.defence / 100);
+      this.health -= points * (1 - (this.defence / 100));
     }
   }
 }
