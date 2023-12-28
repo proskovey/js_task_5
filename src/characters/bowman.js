f64b298f8e1cae0fba0a70d834fa5543a84136f3
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:13:00 +0500

    Update bowman.js

--- src/characters/bowman.js
+++ src/characters/bowman.js
@@ -1,9 +1,9 @@
 import Character from './character';
 
-class Bowman extends Character {
-    constructor(name) {
-        super(name);
-        this.attack = 25;
-        this.defence = 25;
-    }
+export default class Bowerman extends Character {
+  constructor(name, type = 'Bowman') {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
 }
