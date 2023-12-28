d91669c86de09b272ed1af012dafe527e860393f
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:29:21 +0500

    Update swordsman.test.js

--- src/characters/__tests__/swordsman.test.js
+++ src/characters/__tests__/swordsman.test.js
@@ -1,16 +1,16 @@
 import Swordsman from '../swordsman';
 
 test('instance of the class Swordsman', () => {
-  const person = new Swordsman('luba');
+  const person = new Swordsman('dany');
   expect(person).toEqual({
-    health: 100, level: 1, attack: 40, defence: 10, type: 'Swordsman', name: 'luba',
+    health: 100, level: 1, attack: 40, defence: 10, type: 'Swordsman', name: 'dany',
   });
 });
 
 test('levelUp for class', () => {
-  const person = new Swordsman('luba');
+  const person = new Swordsman('dany');
   person.levelUp();
   expect(person).toEqual({
-    health: 100, level: 2, attack: 48, defence: 12, name: 'luba', type: 'Swordsman',
+    health: 100, level: 2, attack: 48, defence: 12, name: 'dany', type: 'Swordsman',
   });
 });
