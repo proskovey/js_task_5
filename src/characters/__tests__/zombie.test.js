68bf14a021e0031403cbbc1236175e2348b21d1a
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:33:34 +0500

    Update zombie.test.js

--- src/characters/__tests__/zombie.test.js
+++ src/characters/__tests__/zombie.test.js
@@ -1,16 +1,16 @@
 import Zombie from '../zombie';
 
 test('instance of the class Zombie', () => {
-  const person = new Zombie('luba');
+  const person = new Zombie('dany');
   expect(person).toEqual({
-    health: 100, level: 1, attack: 40, defence: 10, type: 'Zombie', name: 'luba',
+    health: 100, level: 1, attack: 40, defence: 10, type: 'Zombie', name: 'dany',
   });
 });
 
 test('levelUp for class', () => {
-  const person = new Zombie('luba');
+  const person = new Zombie('dany');
   person.levelUp();
   expect(person).toEqual({
-    health: 100, level: 2, attack: 48, defence: 12, name: 'luba', type: 'Zombie',
+    health: 100, level: 2, attack: 48, defence: 12, name: 'dany', type: 'Zombie',
   });
 });
