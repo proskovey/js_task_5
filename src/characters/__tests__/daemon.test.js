5bb53deaa40534fd974ee0463225d13d3bb62321
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:28:13 +0500

    Update daemon.test.js

--- src/characters/__tests__/daemon.test.js
+++ src/characters/__tests__/daemon.test.js
@@ -1,16 +1,16 @@
 import Daemon from '../daemon';
 
 test('instance of the class Daemon', () => {
-  const person = new Daemon('luba');
+  const person = new Daemon('dany');
   expect(person).toEqual({
-    health: 100, level: 1, attack: 10, defence: 40, type: 'Daemon', name: 'luba',
+    health: 100, level: 1, attack: 10, defence: 40, type: 'Daemon', name: 'dany',
   });
 });
 
 test('levelUp for class', () => {
-  const person = new Daemon('luba');
+  const person = new Daemon('dany');
   person.levelUp();
   expect(person).toEqual({
-    health: 100, level: 2, attack: 12, defence: 48, name: 'luba', type: 'Daemon',
+    health: 100, level: 2, attack: 12, defence: 48, name: 'dany', type: 'Daemon',
   });
 });
