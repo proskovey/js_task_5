9dcd5ba007d4e80e6a8abd55ab1dd739fa8003f8
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 11:27:36 +0500

    Update bowman.js

--- src/characters/bowman.js
+++ src/characters/bowman.js
@@ -2,6 +2,8 @@ import Character from './character';
 
 class Bowman extends Character {
     constructor(name) {
-        super(name, 25, 25);
+        super(name);
+        this.attack = 25;
+        this.defence = 25;
     }
 }
