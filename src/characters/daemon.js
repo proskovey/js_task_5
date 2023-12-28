84b01055745181f9f5e2ed77e6e8a7fca6d21357
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:14:01 +0500

    Update daemon.js

--- src/characters/daemon.js
+++ src/characters/daemon.js
@@ -1,9 +1,9 @@
 import Character from './character';
 
-class Daemon extends Character {
-    constructor(name) {
-        super(name);
-        this.attack = 10;
-        this.defence = 40;
-    }
+export default class Daemon extends Character {
+  constructor(name, type = 'Daemon') {
+    super(name, type);
+    this.attack = 10;
+    this.defence = 40;
+  }
 }
