37cabcecd02e89365d99f3705afd78ae2338b669
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:27:41 +0500

    Update bowerman.test.js

--- src/characters/__tests__/bowerman.test.js
+++ src/characters/__tests__/bowerman.test.js
@@ -1,16 +1,16 @@
 import Bowerman from '../bowerman';
 
 test('instance of the class Bowerman', () => {
-  const person = new Bowerman('luba');
+  const person = new Bowerman('dany');
   expect(person).toEqual({
-    health: 100, level: 1, attack: 25, defence: 25, type: 'Bowman', name: 'luba',
+    health: 100, level: 1, attack: 25, defence: 25, type: 'Bowman', name: 'dany',
   });
 });
 
 test('levelUp for class', () => {
-  const person = new Bowerman('luba');
+  const person = new Bowerman('dany');
   person.levelUp();
   expect(person).toEqual({
-    health: 100, level: 2, attack: 30, defence: 30, name: 'luba', type: 'Bowman',
+    health: 100, level: 2, attack: 30, defence: 30, name: 'dany', type: 'Bowman',
   });
 });
