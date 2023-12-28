ccd7aad4b9eb0da6b825c0b67e49245cd98568aa
proskovey <93362757+proskovey@users.noreply.github.com>
2023-12-28 14:15:55 +0500

    Update undead.js

--- src/characters/undead.js
+++ src/characters/undead.js
@@ -1,9 +1,9 @@
 import Character from './character';
 
-class Undead extends Character {
-    constructor(name) {
-        super(name);
-        this.attack = 25;
-        this.defence = 25;
-    }
+export default class Undead extends Character {
+  constructor(name, type = 'Undead') {
+    super(name, type);
+    this.attack = 25;
+    this.defence = 25;
+  }
 }
